827b1c1063d4d5ddf7f68cb5c58dab5bc5db71d7
zappays <yuta.shinoza@zappa.co.jp>
2022-10-17 15:49:19 +0900

    2重送信防止機能追加

---
+++ wwwroot/lib/jquery/dist/nijusousin.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $("form").on("submit", function (e) {
+        var $form = $(this);
+        // クライアントバリデーションに成功したら submit を無効にする
+        if ($form.valid()) {
+            $(this).find("[data-disable-with]").each(function () {
+                var $button = $(this);
+                var text = $button.attr("data-disable-with");
+                $button.val(text);
+                $button.prop("disabled", true);
+            });
+        }
+    });
+});
