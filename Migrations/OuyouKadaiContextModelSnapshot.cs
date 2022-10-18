1d435e5e74e6ffbe55bef0b28768af7dc11bd71c
zappays <yuta.shinoza@zappa.co.jp>
2022-10-18 18:24:47 +0900

    単体テスト資料追加

--- Migrations/OuyouKadaiContextModelSnapshot.cs
+++ Migrations/OuyouKadaiContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace OuyouKadai.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.9")
+                .HasAnnotation("ProductVersion", "6.0.10")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("OuyouKadai.Models.Auth", b =>
